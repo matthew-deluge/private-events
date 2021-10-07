e6526d730feb9344419a2f512afdba011420eee2
matthew-deluge <matthewj913@gmail.com>
2021-10-07 17:58:16 -0400

    Wrote first events create form and updated controller to allow creation params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.fetch(:event, {})
+      params.require(:event).permit(:title, :description, :starts_time, :location)
     end
 end
