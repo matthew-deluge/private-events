940968e8aa3a2e9cf9a9b704640f411c85590cbb
matthew-deluge <matthewj913@gmail.com>
2021-10-11 18:34:15 -0400

    Added show page for users that displays their events, and usernames on the index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, foreign_key: "creator_id", class_name: "events"
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   
 end
