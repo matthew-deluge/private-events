6bb113390eec56e7abea7b8c453a796d07438be5
matthew-deluge <matthewj913@gmail.com>
2021-10-31 18:32:21 -0400

    added scopes for upcoming and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :sign_ups, foreign_key: :event_attended_id
   has_many :attendees, through: :sign_ups
+
+  scope :past, -> { where('starts_time < ?', Date.today) }
+  scope :upcoming, -> { where('starts_time > ?', Date.today) }
 end
